e130246f2b71c991dd8fef933d73cba833a73f2c
RaccoonGem <jack@honeydog.org>
2023-07-13 16:14:20 -0500

    Add ETL code

--- src/schema.sql
+++ src/schema.sql
@@ -3,39 +3,63 @@ CREATE DATABASE ratingsandreviews;
 USE ratingsandreviews;
 
 CREATE TABLE reviews (
-  id int NOT NULL AUTO_INCREMENT,
-  product_id int NOT NULL,
-  rating int NOT NULL,
-  `date` int NOT NULL,
-  summary varchar(60) NOT NULL,
-  body varchar(100) NOT NULL,
-  recommend boolean NOT NULL,
-  reported boolean NOT NULL,
-  reviewer_name varchar(60) NOT NULL,
-  reviewer_email varchar(60) NOT NULL,
-  response varchar(200),
-  helpfulness int NOT NULL DEFAULT 0,
-  PRIMARY KEY (ID)
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  rating INT NOT NULL,
+  `date` DATE NOT NULL,
+  summary VARCHAR(60) NOT NULL,
+  body VARCHAR(100) NOT NULL,
+  recommend BOOLEAN NOT NULL,
+  reported BOOLEAN NOT NULL,
+  reviewer_name VARCHAR(60) NOT NULL,
+  reviewer_email VARCHAR(60) NOT NULL,
+  response VARCHAR(200) DEFAULT NULL,
+  helpfulness INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE reviews_photos (
-  id int NOT NULL AUTO_INCREMENT,
-  review_id int NOT NULL,
-  `url` varchar(300),
-  PRIMARY KEY (ID)
+  id INT NOT NULL AUTO_INCREMENT,
+  review_id INT NOT NULL,
+  `url` VARCHAR(300),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE characteristics (
-  id int NOT NULL AUTO_INCREMENT,
-  product_id int NOT NULL,
-  `name` varchar(7) NOT NULL,
-  PRIMARY KEY (ID)
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  `name` VARCHAR(7) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE characteristics_reviews (
-  id int NOT NULL AUTO_INCREMENT,
-  characteristic_id int NOT NULL,
-  review_id int NOT NULL,
-  `value` int NOT NULL DEFAULT 0,
-  PRIMARY KEY (ID)
+  id INT NOT NULL AUTO_INCREMENT,
+  characteristic_id INT NOT NULL,
+  review_id INT NOT NULL,
+  `value` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
 );
+
+LOAD DATA INFILE '../../reviews.csv'
+INTO TABLE reviews
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '../../reviews_photos.csv'
+INTO TABLE reviews_photos
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '../../characteristics.csv'
+INTO TABLE characteristics
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '../../characteristics_reviews.csv'
+INTO TABLE characteristics_reviews
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
